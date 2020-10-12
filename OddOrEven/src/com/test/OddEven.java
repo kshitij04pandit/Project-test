a60a0bb0a4ed2743f5304f21ebc6b1b838829d87
Kshitij <Kshitij@DESKTOP-TF5OO07>
2020-10-12 17:30:10 +0530

    OddEven Code

---
+++ OddOrEven/src/com/test/OddEven.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = Integer.parseInt(args[0]);
+		
+		if ( i%2 == 0)
+			System.out.println("The given number " + i + " is EVEN.");
+			
+		else
+			System.out.println("The given number " + i + " is ODD.");
+
+	}
+
+}
